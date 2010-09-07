0b14b03cec5d7c4ad782992971a5a361f24ef986
flop25 <flop25@piwigo.org>
2010-09-07 13:04:35 +0000

    git-svn-id: http://piwigo.org/svn/extensions/floPure/Pure_grey@6863 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themeconf.inc.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Theme Name: Pure_grey
+Version: 1.0.0
+Description: a subtle  mix of grey inspired web 2.0.
+Theme URI: http://fr.piwigo.org/ext/extension_view.php?eid=445
+Author: flop25
+Author URI: http://www.planete-flop.fr/site.php?acc=file
+*/
+
+$themeconf = array(
+  'name'         => 'Pure_grey',
+  'parent'        => 'Pure_default',
+  'icon_dir'      => 'themes/Pure_default/icon',
+  'mime_icon_dir' => 'themes/Pure_default/icon/mimetypes/',
+);
+
+?>
